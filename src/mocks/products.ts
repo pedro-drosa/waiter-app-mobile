2a18b3ed3485eb9a353b59598981bfd3f1ac9444
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-17 14:55:25 -0300

    fix: change imagePath

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -3,7 +3,7 @@ export const products = [
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: '6ddaa2674fae50ee79d9922c53bdf9e4-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
@@ -32,7 +32,7 @@ export const products = [
     _id: '6372e276a381106c0f854cb3',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
+    imagePath: 'e4a817917a7d291ee2939e2276eede12-coca-cola.png',
     price: 7,
     ingredients: [],
   },
