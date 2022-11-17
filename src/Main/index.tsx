3b1a58a9ba07200b530a2f04b58f12fc3eeff233
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-17 15:47:15 -0300

    feat: add components on home screen

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,10 +1,32 @@
+import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
-import { Container } from './styles';
+import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer,
+} from './styles';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer>
+          <Button onPress={() => alert('Opa')}>Novo Pedido</Button>
+        </FooterContainer>
+      </Footer>
+    </>
   );
 }
