a869e319fef918446acedaacdbb64ac8024893d5
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-17 14:53:14 -0300

    feat: format currency in BRL pattern

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { Main } from './src/Main';
 
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
