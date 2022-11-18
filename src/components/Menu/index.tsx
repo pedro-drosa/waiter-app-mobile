57ea5ed3faf5d3a3b88faf9d98cdb45fdcdcdb70
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-18 09:57:06 -0300

    feat(ProductModal.tsx): create component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,47 +1,65 @@
 import { FlatList } from 'react-native';
 import { products } from '../../mocks/products';
 import { Text } from '../Text';
+import { ProductModal } from '../ProductModal';
 import {
-  Product,
   ProductImage,
   ProductDetails,
   Separator,
   AddToCartButton,
+  ProductContainer,
 } from './styles';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { useState } from 'react';
+import { Product } from '../../types/Product';
 
 export function Menu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
   return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      ItemSeparatorComponent={Separator}
-      keyExtractor={(product) => product._id}
-      renderItem={({ item: product }) => {
-        return (
-          <Product>
-            <ProductImage
-              source={{
-                uri: `https://37f6-168-195-96-104.sa.ngrok.io/uploads/${product.imagePath}`,
-              }}
-            />
-            <ProductDetails>
-              <Text weight="600">{product.name}</Text>
-              <Text size={14} color="#666" style={{ marginVertical: 8 }}>
-                {product.description}
-              </Text>
-              <Text size={14} weight="600">
-                {formatCurrency(product.price)}
-              </Text>
-            </ProductDetails>
-            <AddToCartButton>
-              <PlusCircle />
-            </AddToCartButton>
-          </Product>
-        );
-      }}
-    />
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        product={selectedProduct}
+      />
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(product) => product._id}
+        renderItem={({ item: product }) => {
+          return (
+            <ProductContainer onPress={() => handleOpenModal(product)}>
+              <ProductImage
+                source={{
+                  uri: `https://37f6-168-195-96-104.sa.ngrok.io/uploads/${product.imagePath}`,
+                }}
+              />
+              <ProductDetails>
+                <Text weight="600">{product.name}</Text>
+                <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+                  {product.description}
+                </Text>
+                <Text size={14} weight="600">
+                  {formatCurrency(product.price)}
+                </Text>
+              </ProductDetails>
+              <AddToCartButton>
+                <PlusCircle />
+              </AddToCartButton>
+            </ProductContainer>
+          );
+        }}
+      />
+    </>
   );
 }
---
+++ src/components/ProductModal/index.tsx
@@ -0,0 +1,89 @@
+import { FlatList, Modal } from 'react-native';
+import { Product } from '../../types/Product';
+import { Close } from '../Icons/Close';
+import { Text } from '../Text';
+import { formatCurrency } from '../../utils/formatCurrency';
+import {
+  Image,
+  CloseButton,
+  ModalBody,
+  Header,
+  IngredientsContainer,
+  Ingredient,
+  Footer,
+  FooterContainer,
+  PriceContainer,
+} from './styles';
+import { Button } from '../Button';
+
+interface ProductModalProps {
+  visible: boolean;
+  onClose: () => void;
+  product: null | Product;
+}
+
+export function ProductModal({ visible, onClose, product }: ProductModalProps) {
+  if (!product) return null;
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={onClose}
+    >
+      <Image
+        source={{
+          uri: `https://37f6-168-195-96-104.sa.ngrok.io/uploads/${product.imagePath}`,
+        }}
+      >
+        <CloseButton onPress={onClose}>
+          <Close />
+        </CloseButton>
+      </Image>
+      <ModalBody>
+        <Header>
+          <Text size={24} weight="600">
+            {product.name}
+          </Text>
+          <Text color="#666" style={{ marginTop: 8 }}>
+            {product.description}
+          </Text>
+        </Header>
+        {product.ingredients.length > 0 && (
+          <IngredientsContainer>
+            <Text color="#666" weight="600">
+              Ingredientes
+            </Text>
+            <FlatList
+              data={product.ingredients}
+              keyExtractor={(ingredient) => ingredient._id}
+              showsVerticalScrollIndicator={false}
+              style={{ marginTop: 16 }}
+              renderItem={({ item: ingredient }) => (
+                <Ingredient>
+                  <Text>{ingredient.icon}</Text>
+                  <Text color="#666" size={14} style={{ marginLeft: 20 }}>
+                    {ingredient.name}
+                  </Text>
+                </Ingredient>
+              )}
+            />
+          </IngredientsContainer>
+        )}
+      </ModalBody>
+      <Footer>
+        <FooterContainer>
+          <PriceContainer>
+            <Text color="#666">Preço</Text>
+            <Text size={20} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </PriceContainer>
+          <Button onPress={() => alert('Adicionar ao pedido')}>
+            Adicionar ao pedido
+          </Button>
+        </FooterContainer>
+      </Footer>
+    </Modal>
+  );
+}
