03c67599f4a7317bfdfd8bc04bc54135143312ed
pedro-drosa <pedro_drosa@hotmail.com>
2022-11-18 08:22:58 -0300

    feat(Header.tsx): cancel order

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -20,10 +20,17 @@ export function Main() {
     setSelectedTable(table);
   }
 
+  function handleCancelOrder() {
+    setSelectedTable('');
+  }
+
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCacelOrder={handleCancelOrder}
+        />
         <CategoriesContainer>
           <Categories />
         </CategoriesContainer>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,43 @@
-import { Container } from './styles';
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
+import { Container, Content, OrderHeader, Table } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  selectedTable: string;
+  onCacelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCacelOrder }: HeaderProps) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>
-        Bem vindo ao
-      </Text>
-      <Text size={24} weight="700">
-        WAITER
-        <Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>
+            Bem vindo ao
+          </Text>
+          <Text size={24} weight="700">
+            WAITER
+            <Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+      {selectedTable && (
+        <Content>
+          <OrderHeader>
+            <Text size={24} weight="600">
+              Pedido
+            </Text>
+            <TouchableOpacity onPress={onCacelOrder}>
+              <Text color="#D73035" size={14} weight="600">
+                Cancelar Pedido
+              </Text>
+            </TouchableOpacity>
+          </OrderHeader>
+          <Table>
+            <Text color="#666">Mesa: {selectedTable}</Text>
+          </Table>
+        </Content>
+      )}
     </Container>
   );
 }
